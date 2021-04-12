91ffee166653876f51220f63a4774bcd664545ac
Abdilahi Jama <abdilahi.jama@student.moringaschool.com>
2021-04-12 06:28:57 +0200

    Defined new function

--- js/script.js
+++ js/script.js
@@ -1,32 +1,31 @@
-var daysOfTheWeek = ["0" , "1" , "2" , "3", "4" , "5", "6"];
 var maleDays = ["Kwasi" , "Kwadwo" , "Kwabena" , "Kwaku" , "Yaw" , "Kofi" , "Kwame"];
 var femaleDays = ["Akosua" , "Adwoa" , "Abenaa" , "Akua" , "Yaa" , "Afua","Ama"];
-var day = document.getElementById('day').innerHTML;
-var month = document.getElementById('month').innerHTML;
-var century = document.getElementById('century').innerHTML;
-var decade = document.getElementById('decade').innerHTML;
-var female = document.getElementById('female').innerHTML;
-var male = document.getElementById('male').innerHTML;
-var gender = document.getElementById('gender').innerHTML;
-
-if(day <= 0 || > 31) {
-
-  console.log("Inavlid entry");
-}
-
-if(month <= 0 || > 12) {
-  console.log("Invalid entry!");
-}
 
 function dayCalculator(day , month , century , decade) {
   var cc = (((century / 4) - 2 * century - 1));
   var yy = ((5 * decade / 4));
   var mmdd = ((26 *(month + 1) / 10)) + day;
   var total = (cc + yy + mmdd) % 7;
+  return total;
 }
 
-function femaleDays (gender) {
-  if(gender === female) {
+function determineName() {
+  let x;
+  var day = document.getElementById('day').value;
+  var month = document.getElementById('month').value;
+  var century = document.getElementById('century').value;
+  var decade = document.getElementById('decade').value;
+  let dayCalc = dayCalculator(day,month,century,decade);
+  var gender = document.getElementById('gender').value;
+  if(gender == "female") {
+    x = femaleDays[dayCalc];
+  }  else {
+    x = maleDays[dayCalc];
+  }
+  function maxMonth() {
 
   }
+   console.log(x);
+   console.log(dayCalc);
+
 }
