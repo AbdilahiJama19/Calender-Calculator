98b57c4fc4490ad25d5db7fc067c3afc481507e8
Abdilahi Jama <abdilahi.jama@student.moringaschool.com>
2021-04-12 20:11:47 +0200

    Debugged some errors

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 function dayCalculator(dd, mm, yy, cc) {
-  const akanDay = (((century / 4) - 2*century - 1) +((5*decade / 4)) + ((26*(month + 1) / 10)) + day) % 7;
+  const akanDay = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
   return akanDay.toFixed();
 }
 
 function maxMonths(days, months) {
-  if(days <= 0  && days > 31){
-    alert("Invalid Entry");
+  if (days <= 0 && days > 31) {
+    alert('Invalid Entry');
+  } else if (months <= 0 && months > 12) {
+    alert('Invalid Entry');
   }
-  else if(months <= 0 && months > 12) {
-    alert("Invalid Entry");
-  }
+}
+
 function determineName() {
   let x;
-  var maleDays = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-  var femaleDays = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua','Ama'];
-  const day = parseInt(document.getElementById('day').value);
-  const month = parseInt(document.getElementById('month').value);
+  const maleDays = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+  const femaleDays = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+
   const century = parseInt(document.getElementById('century').value);
   const decade = parseInt(document.getElementById('decade').value);
+  const month = parseInt(document.getElementById('month').value);
+  const day = parseInt(document.getElementById('day').value);
 
-  let dayCalc = dayCalculator(day, month,decade, century);
+  let dayCalc = dayCalculator(day, month, decade, century);
   const gender = document.getElementById('gender').value;
-  =if(gender ==+ "female") {
-    x = femaleDays[dayCalc]
-  }  else {
+
+  if (gender === 'female') {
+    x = femaleDays[dayCalc];
+  } else {
     x = maleDays[dayCalc];
   }
   document.getElementById('output-name').innerHTML = x;
-}
-
-
 }
